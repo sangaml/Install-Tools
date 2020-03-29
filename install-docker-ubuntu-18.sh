dc6064178fb862d7f562504e07afe209100737bd
sangaml <sangamlonk@gmail.com>
2020-03-29 16:23:01 +0530

    install docker

--- install-docker-ubuntu-18.sh
+++ install-docker-ubuntu-18.sh
@@ -14,9 +14,9 @@ sudo apt update -y >>$LOGFILE 2>>$ERRORFILE
 
 echo "Installing Docker..."
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y >>$LOGFILE 2>>$ERRORFILE \
-&& echo | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >>$LOGFILE 2>>$ERRORFILE \
-&& sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" >>$LOGFILE 2>>$ERRORFILE \
-&& sudo apt update -y >>$LOGFILE 2>>$ERRORFILE \
-&& sudo apt install docker-ce -y >>$LOGFILE 2>>$ERRORFILE \
-&& sudo usermod -aG docker ${USER} >>$LOGFILE 2>>$ERRORFILE 
+echo | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >>$LOGFILE 2>>$ERRORFILE \
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" >>$LOGFILE 2>>$ERRORFILE \
+sudo apt update -y >>$LOGFILE 2>>$ERRORFILE \
+sudo apt install docker-ce -y >>$LOGFILE 2>>$ERRORFILE \
+sudo usermod -aG docker ${USER} >>$LOGFILE 2>>$ERRORFILE 
 echo "Installed Docker ...:)"
